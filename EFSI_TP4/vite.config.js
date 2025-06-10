3383a0f762dca0aa46aacd594bc6fb1731c95104
MARCO DI CARLO <48115731@ort>
2025-06-10 08:38:26 -0300

    git pages

--- EFSI_TP4/vite.config.js
+++ EFSI_TP4/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EFSI_TP4_Di_Carlo/',
+  homepage:
+  "https://github.com/Markoss5/EFSI_TP4_Di_Carlo"
 })
